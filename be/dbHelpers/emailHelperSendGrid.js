c9a448051ff9d3627187c85b0e9c6567806aecdc
carlodev <johncarloaganan26@gmail.com>
2026-01-07 03:39:03 +0800

    Update church website URL in email templates

--- be/dbHelpers/emailHelperSendGrid.js
+++ be/dbHelpers/emailHelperSendGrid.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // Church branding constants
 const CHURCH_NAME = 'Bible Baptist Ekklesia of Kawit';
 const CHURCH_EMAIL = process.env.SENDGRID_FROM_EMAIL || 'babyblissbooking@gmail.com';
-const CHURCH_WEBSITE = 'https://bbek-kawit.com';
+const CHURCH_WEBSITE = 'https://bbek-church-app.vercel.app';
 const CHURCH_PHONE = '(046) 123-4567';
 const CHURCH_ADDRESS = 'Kawit, Cavite, Philippines';
 
