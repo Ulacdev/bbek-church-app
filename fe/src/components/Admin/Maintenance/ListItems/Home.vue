f8ed40f39c8572623721cd22a577b500a004e3fa
John Carlo <johncarlo@example.com>
2026-02-01 05:19:21 +0800

    Add YouTube embed URL example to CMS

--- fe/src/components/Admin/Maintenance/ListItems/Home.vue
+++ fe/src/components/Admin/Maintenance/ListItems/Home.vue
@@ -67,7 +67,7 @@
       <div class="item-action">
         <el-input
           v-model="homeData.homeVideo"
-          placeholder="Enter YouTube/Vimeo embed URL (e.g., https://www.youtube.com/embed/...)"
+          placeholder="Enter YouTube/Vimeo embed URL (e.g., https://www.youtube.com/embed/dQw4w9WgXcQ)"
           clearable
           style="width: 400px;"
         />
@@ -84,7 +84,7 @@
       </div>
       <div class="text-info" style="margin-top: 8px; font-size: 12px;">
         <el-icon><InfoFilled /></el-icon>
-        Tip: Use YouTube embed URL for best compatibility. Example: https://www.youtube.com/embed/VIDEO_ID
+        Tip: Use YouTube embed URL. Example: https://www.youtube.com/embed/dQw4w9WgXcQ
       </div>
     </div>
     <el-divider v-if="homeData.backgroundType === 'video'" />
